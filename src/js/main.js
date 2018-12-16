a8f144e1bd61fb10e67673c5c83ccd9171a9f686
Sergey Khmelevskoy <xs290@me.com>
2018-12-17 00:18:32 +0300

    ie fixes

--- src/js/main.js
+++ src/js/main.js
@@ -100,6 +100,33 @@ $(document).ready(function(){
   // COMMON
   //////////
 
+  // detectors
+  function isRetinaDisplay() {
+    if (window.matchMedia) {
+        var mq = window.matchMedia("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");
+        return (mq && mq.matches || (window.devicePixelRatio > 1));
+    }
+  }
+
+  function isMobile(){
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  function msieversion() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+      return true
+    } else {
+      return false
+    }
+  }
+
   function legacySupport(){
     // svg support for laggy browsers
     svg4everybody();
@@ -110,6 +137,15 @@ $(document).ready(function(){
       refreshDebounceWait: 150,
       appendToBody: true
     });
+
+    if ( msieversion() ){
+      $('body').addClass('is-ie');
+    }
+
+    if ( isMobile() ){
+      $('body').addClass('is-mobile');
+    }
+
   }
 
   // preloader
